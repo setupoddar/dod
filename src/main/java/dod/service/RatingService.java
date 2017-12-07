2c4ec5bab9abd0e56ae6e9982b374fa4b5630e8a
setu.poddar <setu.poddar@flipkart.com>
2017-12-08 02:17:05 +0530

    Adding code

--- src/main/java/dod/service/RatingService.java
+++ src/main/java/dod/service/RatingService.java
@@ -2,24 +2,29 @@ package dod.service;
 
 import com.google.inject.Inject;
 import dod.dal.dao.ProductDAO;
+import dod.dal.model.Product;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class RatingService {
 
     protected ProductDAO productDAO;
+    private Integer threshold = 4;
 
     @Inject
     public RatingService(ProductDAO productDAO) {
         this.productDAO = productDAO;
     }
 
-    public Map<String,String> getRatingTags(List<String> productIds){
-
-
-        productDAO.getProducts(productIds);
-        return null;
-
+    public Map<String, String> getRatingTags(List<String> productIds) {
+        Map<String, String> productRatingMap = new HashMap<String, String>();
+        List<Product> products = productDAO.getProducts(productIds);
+        for (Product product : products) {
+            if (product.getRating() > threshold)
+                productRatingMap.put(product.getId(), "4+ Rated");
+        }
+        return productRatingMap;
     }
 }
