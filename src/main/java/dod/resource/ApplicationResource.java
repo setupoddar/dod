978f1c1881ee1b98bc3d35bd4ce724e74623f859
manas-kirti <manas.kirti@flipkart.com>
2017-12-08 03:54:34 +0530

    Resource file

--- src/main/java/dod/resource/ApplicationResource.java
+++ src/main/java/dod/resource/ApplicationResource.java
@@ -1,11 +1,13 @@
 package dod.resource;
 
+import com.google.inject.Inject;
+import dod.service.PricingService;
+import io.dropwizard.jersey.params.LongParam;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
+import com.google.inject.Provider;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -18,7 +20,18 @@ import javax.ws.rs.core.MediaType;
 @Slf4j
 public class ApplicationResource {
 
+    private final Provider<PricingService> pricingServiceProvider;
 
+    @Inject
+    public ApplicationResource(Provider<PricingService> pricingServiceProvider) {
+        this.pricingServiceProvider = pricingServiceProvider;
+    }
 
-
+    @GET
+    @Path("/price/low/listingId/{listingId}/time/{days}")
+    public Long getLowestPriceInXDaysTag(@PathParam("listingId")String listingId, @PathParam("days")LongParam days){
+        log.info("getLowestPriceInXDaysTag for listing id {} and days {}", listingId, days.get());
+        //TODO Change it to proper day to millis conversion
+        return pricingServiceProvider.get().getLeastPriceInRange(listingId, days.get() * 1000000);
+    }
 }
